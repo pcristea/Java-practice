0a1b532cb902b0aa5bc19d7ff7a9a006474ab8bc
Petru Cristea <ppcristea@gmail.com>
2016-10-01 17:15:42 +0300

    home01

---
+++ src/com/javarush/test/level04/lesson06/task05/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level04.lesson06.task05;
+
+/* 18+
+Ввести с клавиатуры имя и возраст. Если возраст меньше 18 вывести надпись «Подрасти еще».
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        int i = scanner.nextInt();
+        if(i < 18)
+            System.out.println("Подрасти еще");
+
+
+    }
+}
